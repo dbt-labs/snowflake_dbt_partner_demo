2c19baa11f695fd0af9f8fc6945b9a9aa27f5f89
Hope Watson <hope.watson@fishtownanalytics.com>
2023-09-06 18:33:35 +0000

    update the column names to reflect the underlying data in snowflake

--- models/demo_examples/dynamic_table.sql
+++ models/demo_examples/dynamic_table.sql
@@ -10,11 +10,11 @@
 
 select 
 
-  date_part('month', orders.order_date) as order_month,
+  date_part('month', orders.ordered_at) as order_month,
   payments.payment_method,
   sum(payments.amount) as total_revenue
 
 from  {{ ref('stg_stripe_payments') }} payments
 inner join {{ ref('stg_jaffle_orders') }} orders on (payments.order_id = orders.order_id)
-where orders.status = 'completed'
+where payments.status = 'completed'
 group by order_month, payment_method
